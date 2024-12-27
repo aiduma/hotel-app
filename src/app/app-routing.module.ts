42b2872467c7f15a1a82b624063ac147e7ed113c
aiduma <aiduma08@gmail.com>
2024-12-27 08:23:47 -0700

    Added Code to List, Create, Edit and Deletete Reservations using LocalStorage.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReservationFormComponent } from './reservation-form/reservation-form.component';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
+import { HomeModule } from './home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: HomeComponent},
+  {path:"list", component: ReservationListComponent},
+  {path:"new", component: ReservationFormComponent},
+  {path:"edit/:id", component: ReservationFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
